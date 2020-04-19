c4bc0e885b4506a9894e44d47a2f33f3d5187e92
Arrow2dknee <yozziedummy@gmail.com>
2020-04-19 17:03:06 +0530

    Cats feature module

---
+++ src/common/middleware/logger.middleware.ts
@@ -0,0 +1,12 @@
+// Class based middleware
+
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+    use(req: Request, res: Response, next: Function) {
+        console.log('logger-middleware');
+        next();
+    }
+}
